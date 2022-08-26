e86c84e9f2a2ee5382d2c125b3d4f1fd4f52c836
Ph4nt0m <53800198+Dave-ss@users.noreply.github.com>
2022-08-26 17:21:37 -0300

    atualizando classe leitor

--- CsvReader.rb
+++ CsvReader.rb
@@ -1,85 +1,44 @@
 require 'csv'
-# require 'pry'
-require_relative 'Aluno'
-require_relative 'Curso'
+require_relative 'Universidade'
+
 class Leitor
-
-
-  def initialize
-    @alunos = []
-    @cursos = []
-    @arquivo = CSV.parse(File.read('notas.csv'), headers: true)
+  def initialize(nome_arquivo)
+    @universidade = Univesidade.new
+    @nome_arquivo = nome_arquivo
+    @arquivo =  CSV.parse(File.read(nome_arquivo), headers: true)
     rescue Exception => exception
       raise Exception, "Falha ao ler o arquivo #{exception}"
       exit(1)
   end
 
+  attr_reader :nome_arquivo
   attr_reader :arquivo
-  attr_reader :alunos
-  attr_reader :cursos
+  attr_reader :universidade
 
-  def separar_aluno
-    alunos = []
+  def agrupa_dados
     File.open('notas.csv') do |file|
-      dados = CSV.parse(File.read(file, encoding: 'bom|utf-8'), col_sep: ',', headers: true)
-      dados = dados.group_by { |row| row['MATRICULA'] }
-      dados.each { |k,v| alunos << Aluno.new(k,v) }
-      alunos.each { |aluno| aluno.calcula_cr }
-      # dados.each { |k|
-      #   p k.to_h
-      #   break }
-      #
-      # #
-      # i = 0
-      # t =  true
-      # vet = []
-      # alunos = []
-      # while t
-      #   if dados[i] == nil
-      #     t = false
-      #   elsif if (i > 0 and dados[i]["MATRICULA"] == dados[i-1]["MATRICULA"]) or
-      #     (i == 0 and dados[i]["MATRICULA"] == dados[i+1]["MATRICULA"])
-      #             vet << dados[i]
-      #         else
-      #           aluno = Aluno.new(vet)
-      #           alunos << aluno
-      #           vet = []
-      #           #para ele nao perder a contagem
-      #           if i > 2 and vet.size != 0
-      #             i-=1
-      #           end
-      #         end
-      #   end
-      #   i += 1
-      # end
-      # #
-      # vet << dados[-1]
-      # aluno = Aluno.new(vet)
-      # alunos << aluno
-      # alunos.each { |aluno|  p aluno.calcula_cr
-      # break}
-      #parar
+      dado_bruto = CSV.parse(File.read(file, encoding: 'bom|utf-8'), col_sep: ',', headers: true)
+      dados_aluno = dado_bruto.group_by { |row| row['MATRICULA'] }
+      dados_aluno.each { |k,v| universidade.insere_aluno(k,v) }
+      dados_curso = dado_bruto.group_by { |row| row['COD_CURSO'] }
+      dados_curso.each { |k,v|
+        alunos = []
+        teste = v.group_by{ |row| row['MATRICULA'] }
+        teste.each { |x,y| alunos << x
+        }
+        universidade.insere_curso(k, alunos)
+        }
     end
   end
 
-
-  def separar_curso
-    alunos = []
-    File.open('notas.csv') do |file|
-      dados = CSV.parse(File.read(file, encoding: 'bom|utf-8'), col_sep: ',', headers: true)
-      dados = dados.group_by { |row| row['COD_CURSO'] }
-      dados.each { |k,v| alunos << Aluno.new(k,v)}
-      p alunos
-      # curso = Curso.new(alunos[0], alunos)
-
-
-
-
-    end
-
+  def printa_crs
+    universidade.calcula_cr
+    universidade.printa_cr
   end
+
+  def media_cursos
+    universidade.calcula_cr
+    universidade.media_cursos
+  end
+
 end
-
-p = Leitor.new()
-p.separar_aluno
-p.separar_curso
